c184ba778e10f987a7eb789e200923be328c11bd
azzamaflah <azzamhebat46@gmail.com>
2023-05-25 15:47:29 +0700

    menambahkan linkedlist ke node

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -29,7 +29,7 @@ DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
 
-void DoubleLinkedList::addNode() {
+void DoubleLinkedList::addNode() { 
 	int nim;
 	string nm;
 	cout << "\nEnter the roll number of the student: ";
@@ -40,4 +40,26 @@ void DoubleLinkedList::addNode() {
 	newNode->noMhs = nim; // step 2
 	newNode->name = nm; // step 2
 
+	/* insert a node in the beginning of a doubly - linked list */
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START; //step3
+		if (START != NULL)
+			START->prev = newNode;//step 4
+		newNode->prev = NULL; //steep 5
+		START = newNode; //steep 6
+		return;
+	}
+
+	/*Inserting a Node Between Two Nodes in the List*/
+	Node* current = START; // step 1.a
+	Node* previous = NULL; // step 1.b
+	while (current->next != NULL && current->next->noMhs < nim) // step 1.c
+
+	{
+
+	}
 }
